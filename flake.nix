f975e7811609b5af390502327db07c31720982a9
niloofar-samei <niloofar.samei@gmail.com>
2024-05-26 21:24:03 +0100

    Add stylix.

--- flake.nix
+++ flake.nix
@@ -5,15 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    stylix = { url = "github:danth/stylix";  };
     fg42.url = "git+https://devheroes.codes/FG42/FG42";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, fg42, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, fg42, stylix, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          ./stylix.nix
+          stylix.nixosModules.stylix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
