cf47a232b3cb88fc6423e0ae27883489a1f3b2d3
niloofar-samei <niloofar.samei@gmail.com>
2024-05-31 20:22:09 +0100

    Change unstable to version 24.05.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix = { url = "github:danth/stylix";  };
