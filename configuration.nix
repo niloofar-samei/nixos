0db7a83544507f39ff30dd115d53dd625225e618
niloofar-samei <niloofar.samei@gmail.com>
2024-09-15 20:07:59 +0100

    Cleaneup configuration.nix.

--- configuration.nix
+++ configuration.nix
@@ -1,152 +1,156 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    [./hardware-configuration.nix];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = "nixos"; # Define your hostname.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/London";
 
-  programs.hyprland = {
+  programs = {
+   
+     hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+   
+      shellInit = "eval \"$(direnv hook zsh)\"";
+ 
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" "lein" "thefuck" ];
+        theme = "bira";
+      };
+    };
+  
+  };
+
+  xdg.portal = {
     enable = true;
-    xwayland.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  i18n = {
+    # Select internationalisation properties.
+    defaultLocale = "en_GB.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
   };
 
-  services.v2raya.enable = true;
+  services = {
+    v2raya.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    # Enable the X11 windowing system.
+    xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us,ir";
-    xkb.variant = "dvorak,";
-    xkb.options = "grp:shifts_toggle";
+    # Configure keymap in X11
+    xserver = {
+      xkb.layout = "us,ir";
+      xkb.variant = "dvorak,";
+      xkb.options = "grp:shifts_toggle";
+    };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    udisks2.enable = true;
+  
+    httpd.enable = true;
+    httpd.enablePHP = true;
+
+    httpd.virtualHosts."html" = {
+      documentRoot = "/var/www/html";
+    };
+
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ];
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database  DBuser  auth-method
+        local all       all     trust
+      '';
+    };
+  
   };
-
-  #services.xserver.displayManager.gdm = {
-    #enable = true;
-    #wayland = true;
-  #};
 
   # Configure console keymap
   console.keyMap = "dvorak";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.niloofar = {
-    isNormalUser = true;
-    description = "Niloofar";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      mlocate
-      zsh
-      zsh-autosuggestions
-      zsh-syntax-highlighting
-
-    #  firefox
-    #  kate
-    #  thunderbird
-    ];
-  };
-
-  users.defaultUserShell = pkgs.zsh;
-  users.users.niloofar.shell = pkgs.zsh;
-  environment.shells = with pkgs; [ zsh];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-   
-    shellInit = "eval \"$(direnv hook zsh)\"";
- 
-    ohMyZsh = {
-      enable = true;
-      plugins = [ "git" "lein" "thefuck" ];
-      theme = "bira";
+  users = { 
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.niloofar = {
+      isNormalUser = true;
+      description = "Niloofar";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [
+        mlocate
+        zsh
+        zsh-autosuggestions
+        zsh-syntax-highlighting
+      ];
+      shell = pkgs.zsh;
     };
+   
+    defaultUserShell = pkgs.zsh;
   };
 
-  services.udisks2.enable = true;
-  
-# Allow unfree packages
+  environment.shells = with pkgs; [zsh];
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim 
   ];
 
   fonts.packages = with pkgs; [
@@ -155,53 +159,11 @@
     vazir-fonts
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  
-  #networking.firewall.allowedTCPPorts = [ 80 443 ];
-  services.httpd.enable = true;
-  services.httpd.enablePHP = true;
-
-  services.httpd.virtualHosts."html" = {
-    documentRoot = "/var/www/html";
-  };
-
   systemd.tmpfiles.rules = [
     "d /var/www/html"
     "f /var/www/html/index.php - - - - <?php phpinfo();"
   ];
 
- services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "mydatabase" ];
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-    '';
-  };
-  
-# networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
