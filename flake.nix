73462396c3655374b1607edcd28acb47f3e21882
niloofar-samei <niloofar.samei@gmail.com>
2024-12-20 15:16:59 +0000

    Change nixpkgs release version to 24.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix = { url = "github:danth/stylix";  };
